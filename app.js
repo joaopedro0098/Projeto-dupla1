72cafe1d1ccd8281df1f4a68892593ef8cb53cbd
joaopedro0098 <joaopedro.lemos0098@gmail.com>
2022-11-13 00:30:25 -0300

    verificadas as notas do aluno João

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const gustavo = require('./alunos/gustavo.json')
+const joao = require('./alunos/joao.json')
+
 const verificarNota = require('./verificarNota')
 
 verificarNota(gustavo)
+verificarNota(joao)
